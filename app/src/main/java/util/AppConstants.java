99f094ef828d46997583251acbeae96563f8c19b
Omwitsa <omwitsawilson@yahoo.com>
2022-08-01 14:37:59 +0300

    Save Changes

--- app/src/main/java/util/AppConstants.java
+++ app/src/main/java/util/AppConstants.java
@@ -2,5 +2,5 @@ package util;
 
 public class AppConstants {
     public static final double CONTAINER_WEIGHT = 1.5;
-    public static final String SACCO_CODE = "EMUKA MORINGA FCS";
+    public static final String SACCO_CODE = "KIENI DAIRY PRODUCTS LTD";
 }
